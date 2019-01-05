9b19ed0655888b7f766f2ba2bed967f6ee607e4d
Fried-Chicken <Fried-Chicken@users.noreply.github.com>
2019-01-05 17:50:59 +0100

    Added eslint and jest

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  'parser': 'babel-eslint',
+  'extends': ['airbnb'],
+  'rules': {
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'import/no-extraneous-dependencies': ['error'],
+    'import/prefer-default-export': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'no-console': 'off',
+    'max-len': [
+      'error',
+      {
+        'code': 80,
+        'ignorePattern': 'require',
+      },
+    ],
+  },
+  'env': {
+    'node': true,
+    'jest': true,
+    'es6': true,
+  },
+};
